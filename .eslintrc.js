e31397303c89389a0713d5ccffd22e7ecdbc80b4
Andrey Semenov <semenov.orc@gmail.com>
2024-02-01 08:07:18 +0300

    change .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   overrides: [
     {
       files: ['**/*/*.test.ts'],
       plugins: ['jest'],
       extends: ['plugin:jest/recommended'],
-      rules: { 'jest/prefer-expect-assertions': 'off' }
-    }
+      rules: { 'jest/prefer-expect-assertions': 'off' },
+    },
   ],
-  rules: {}
-}
+  rules: {},
+};
+
